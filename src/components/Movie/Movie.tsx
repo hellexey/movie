6dc882fe2209a4947c0f2f2c291bf75a3315f52b
lesha <hellexey@gmail.com>
2023-06-27 18:22:11 +0500

    test

---
+++ src/components/Movie/Movie.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+
+const Movie = () => {
+  return (
+    <div>
+      <img
+        className="poster"
+        src="https://www.film.ru/sites/default/files/movies/posters/7371606-1210804.jpg"
+        alt="poster"
+      />
+    </div>
+  )
+}
+
+export default Movie
---
+++ src/components/MovieList/MovieList.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './index.css'
+
+import Movie from '../Movie'
+
+const MovieList = () => {
+  return (
+    <div className="movie-list">
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+      <Movie />
+    </div>
+  )
+}
+
+export default MovieList
